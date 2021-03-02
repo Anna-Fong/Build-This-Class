055be633dd50cd4190af9a07938ba97af9e011cc
annafongg <78882159+annafongg@users.noreply.github.com>
2021-03-02 15:56:11 -0800

    2

--- src/Employee.java
+++ src/Employee.java
@@ -1,29 +1,71 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Employee {
 
     private int id;
     private String firstName;
     private String lastName;
     private int salary;
+    private int raiseSalary;
 
-    public void setId(int idNumber) {
-        id = idNumber;
-    }
-    public int getId(){
-        return id;
-    }
+        //id
+        public void setId(int idNumber) {
+            id = idNumber;
+        }
+        public int getId() {
+            return id;
+        }
 
-    public void setFirstName(String firstname) {
-        firstName = firstname;
-    }
-    public String getFirstName(){
-        return firstName;
-    }
+        //first name
+        public void setFirstName(String firstname) {
+            firstName = firstname;
+        }
+        public String getFirstName() {
+            return firstName;
+        }
 
-    public void setLastName(String lastname) {
-        lastName = lastname;
-    }
+        //last name
+        public void setLastName(String lastname) {
+            lastName = lastname;
+        }
+        public String getLastName() {
+            return lastName;
+        }
 
-    public String getLastName() {
-        return lastName;
-    }
+        //full name
+        public String getName() {
+            String getName = firstName + " " + lastName;
+            return getName;
+        }
+
+        //salary
+        public void setSalary(int Salary) {
+            if(Salary < 0) {
+                salary = 0;
+            } else {
+                salary = Salary;
+            }
+        }
+        public int getSalary() {
+            return salary;
+        }
+
+        //annual salary
+        public int getAnnualSalary() {
+            int getAnnualSalary = salary * 12;
+            return getAnnualSalary;
+        }
+        //raised salary (1 month)
+        public void setRaiseSalary(int percent) {
+            int Percent = percent;
+            raiseSalary = ((salary * Percent)/100 + salary);
+        }
+        public int getRaiseSalary() {
+            return raiseSalary;
+
+
+
+
+        }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,30 @@ public class Main {
 
             Employee employee1 = new Employee();
 
-            employee1.setId(5546123);
+            // id
+            employee1.setId(4567456);
             System.out.println(employee1.getId());
 
+            // first name, last name, full name
             employee1.setFirstName("Anna");
             System.out.println(employee1.getFirstName());
 
             employee1.setLastName("Fong");
             System.out.println(employee1.getLastName());
 
+            System.out.println(employee1.getName());
 
+            // monthly salary, annual salary
+            employee1.setSalary(3000);
+            System.out.println(employee1.getSalary());
 
+            System.out.println(employee1.getAnnualSalary());
+
+            // raised salary (one month)
+            employee1.setRaiseSalary(5);
+            System.out.println(employee1.getRaiseSalary());
 
 
         }
-
 }
 
